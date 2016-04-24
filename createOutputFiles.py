8ce321a63044425e06a3695f30127cd5d3f1270a
Jacob C. Levine <jale6295@colorado.edu>
2016-04-24 09:47:34 -0600

    initial commit for createOutputFiles.py.  Changes to getMaxIntensities.py.

---
+++ createOutputFiles.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# File:  createOutputFiles.py
+# Author:  Jacob C. Levine
+# Class Project:  CSCI 4502 Data Mining
+# Date created:  4/24/16
+# Description: 
+#        Creates a fits file called maxIntensities.fits
+#        filled with NaNs located in the 
+#        /home/user/DataMining/Results/ directory. 
+# Arguments: 
+#        wavelength: either 70, 160, 250, 350, or 500
+# Usage:
+#        createOutputFiles.py wavelength
+#
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import sys
+import numpy
+from astropy.io import fits
+
+wavelength = sys.argv[1]
+print "Creating output file for", wavelength, "um wavelength"
+shape = (40501, 6000)
+fill_value = numpy.nan
+dtype = numpy.float32
+order = 'C'
+
+newDataArray = numpy.full(shape, fill_value, dtype, order)
+new_hdu = fits.PrimaryHDU(data=newDataArray)
+
+newFilename = "/home/user/DataMining/Results/" + wavelength + "maxIntensities.fits"
+new_hdu.writeto(newFilename, clobber=True)
+new_hdulist = fits.open(newFilename)
+new_data = new_hdulist[0].data
+new_hdulist.close()
+
